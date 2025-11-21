59c103feef8f3fada916c3d914fa9b976922c8d5
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-11-21 23:05:59 +0100

    WIP: add defi llama oracle

---
+++ crates/bonanca-core/src/api_lib/defi_llama.rs
@@ -0,0 +1,58 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use reqwest::Client;
+use serde::Deserialize;
+use std::collections::HashMap;
+
+use crate::{api_lib::traits::Oracle, holdings::Asset};
+
+pub struct DefiLlama {
+    pub base_url: String,
+}
+
+impl DefiLlama {
+    pub fn new(base_url: String) -> Self {
+        Self { base_url }
+    }
+
+    pub async fn get_price_quote(&self, chain: &str, address: &str) -> Result<LlamaPrice> {
+        let client = Client::new();
+        let url = format!("{}/prices/current/{}:{}", &self.base_url, chain, address);
+
+        let resp = client
+            .get(&url)
+            .header("Accept", "application/json")
+            .send()
+            .await?
+            .json::<LlamaPrice>()
+            .await?;
+
+        Ok(resp)
+    }
+}
+
+#[async_trait]
+impl Oracle for DefiLlama {
+    async fn get_token_value(&self, asset: &Asset, amount: f64) -> Result<f64> {
+        // TODO: change this to use chain:address
+        let quote = self.get_price_quote("coingeco", &asset.name).await?;
+        let price = quote.coins.values().next().unwrap().price;
+        let value = price * amount;
+
+        Ok(value)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LlamaPrice {
+    pub coins: HashMap<String, CoinsData>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CoinsData {
+    pub decimals: u16,
+    pub symbol: String,
+    pub price: f64,
+    pub timestamp: u64,
+    pub confidence: f32,
+}
--- crates/bonanca-core/src/api_lib/mod.rs
+++ crates/bonanca-core/src/api_lib/mod.rs
@@ -1,4 +1,5 @@
 pub mod cmc;
+pub mod defi_llama;
 pub mod jupiter;
 pub mod traits;
 pub mod zerox;

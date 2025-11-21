df15c376b45598002888ad63ba767fa3d7d525c2
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-11-21 13:54:32 +0100

    add tests to keyvault

--- crates/bonanca-keyvault/src/hd_keys.rs
+++ crates/bonanca-keyvault/src/hd_keys.rs
@@ -1,10 +1,17 @@
+use std::str::FromStr;
+
 use alloy::signers::local::PrivateKeySigner;
 use alloy_primitives::FixedBytes;
 use anyhow::Result;
+use argon2::password_hash::SaltString;
 use bip39::{Language, Mnemonic};
 use ed25519_dalek_bip32::{DerivationPath, ExtendedSigningKey};
 use hdwallet::{ExtendedPrivKey, KeyIndex};
-use solana_sdk::signer::keypair::Keypair;
+use rpassword::prompt_password;
+use solana_sdk::signer::{Signer, keypair::Keypair};
+
+use crate::keyvault::{ChainKeys, CipherParams, KdfParams, KeyVault, Vault};
+use crate::utils::{encrypt_seed, hash_password};
 
 pub enum ChildKey {
     Sol(Keypair),
@@ -30,6 +37,59 @@ impl HDkeys {
         let seed = mnemonic.to_seed_normalized("");
 
         Self { seed: seed }
+    }
+
+    pub fn from_mnemonic(mnemonic_str: &str) -> Self {
+        let mnemonic = Mnemonic::from_str(mnemonic_str).unwrap();
+
+        let seed = mnemonic.to_seed_normalized("");
+
+        Self { seed: seed }
+    }
+
+    pub fn get_keyvault(
+        &self,
+        salt: &SaltString,
+        nonce: &str,
+        pass: Option<&str>,
+    ) -> Result<KeyVault> {
+        let password = match pass {
+            Some(txt) => txt.to_string(),
+            None => prompt_password("\nKeyvault Password: ")?,
+        };
+
+        let mac = hash_password(&password, &salt)?;
+
+        let kdf_params = KdfParams {
+            key_length: 32,
+            n: 600_000, // OWASP recommendation
+            salt: salt.as_str().to_string(),
+        };
+
+        let ciphertext = encrypt_seed(self.seed, &password, &nonce, &kdf_params)?;
+
+        let cipher_params = CipherParams {
+            nonce: nonce.to_string(),
+        };
+
+        let vault = Vault {
+            cipher: "aes256-gcm".to_string(),
+            cipher_params: cipher_params,
+            cipher_text: ciphertext,
+            kdf: "pbkdf2".to_string(),
+            kdf_params: kdf_params,
+            mac: mac,
+            salt: salt.as_str().to_string(),
+        };
+
+        let chain_keys = self.create_chain_keys()?;
+
+        let key_vault = KeyVault {
+            valut: vault,
+            chain_keys: chain_keys,
+        };
+
+        Ok(key_vault)
     }
 
     pub fn get_child_key(&self, chain: &str, child: u32) -> Result<ChildKey> {
@@ -59,5 +119,35 @@ impl HDkeys {
             }
             _ => Err(anyhow::anyhow!("Chain not supported")),
         }
+    }
+
+    fn get_child_pubkey(&self, chain: &str, child: u32) -> Result<String> {
+        let child = self.get_child_key(chain, child)?;
+
+        let addy = match child {
+            ChildKey::Sol(kp) => kp.pubkey().to_string(),
+            ChildKey::Evm(sig) => sig.address().to_string(),
+        };
+
+        Ok(addy)
+    }
+
+    pub fn create_chain_keys(&self) -> Result<Vec<ChainKeys>> {
+        let sol_addy = self.get_child_pubkey("Solana", 0)?;
+        let evm_addy = self.get_child_pubkey("EVM", 0)?;
+
+        let sol_keys = ChainKeys {
+            chain: "Solana".to_string(),
+            public_keys: vec![sol_addy],
+        };
+
+        let evm_keys = ChainKeys {
+            chain: "EVM".to_string(),
+            public_keys: vec![evm_addy],
+        };
+
+        let chain_keys = vec![sol_keys, evm_keys];
+
+        Ok(chain_keys)
     }
 }
--- crates/bonanca-keyvault/src/lib.rs
+++ crates/bonanca-keyvault/src/lib.rs
@@ -13,8 +13,8 @@ use std::io::{BufReader, Write};
 use std::path::Path;
 
 use crate::hd_keys::HDkeys;
-use crate::keyvault::{CipherParams, KdfParams, KeyVault, Vault};
-use crate::utils::{create_chain_keys, decrypt_seed, encrypt_seed, hash_password, verify_password};
+use crate::keyvault::KeyVault;
+use crate::utils::{decrypt_seed, verify_password};
 
 pub fn new(filename: &Path, lang: &str) -> Result<()> {
     let language = match lang {
@@ -35,36 +35,8 @@ pub fn new(filename: &Path, lang: &str) -> Result<()> {
     let nonce = hex::encode(nonce);
 
     let hd_key = HDkeys::new(language, word_count);
-    let password = prompt_password("\nKeyvault Password: ").unwrap();
 
-    let mac = hash_password(&password, &salt)?;
-
-    let kdf_params = KdfParams {
-        key_length: 32,
-        n: 600_000, // OWASP recommendation
-        salt: salt.as_str().to_string(),
-    };
-
-    let ciphertext = encrypt_seed(hd_key.seed, &password, &nonce, &kdf_params)?;
-
-    let cipher_params = CipherParams { nonce: nonce };
-
-    let vault = Vault {
-        cipher: "aes256-gcm".to_string(),
-        cipher_params: cipher_params,
-        cipher_text: ciphertext,
-        kdf: "pbkdf2".to_string(),
-        kdf_params: kdf_params,
-        mac: mac,
-        salt: salt.as_str().to_string(),
-    };
-
-    let chain_keys = create_chain_keys(&hd_key)?;
-
-    let key_vault = KeyVault {
-        valut: vault,
-        chain_keys: chain_keys,
-    };
+    let key_vault = hd_key.get_keyvault(&salt, &nonce, None)?;
 
     let contents = serde_json::to_string(&key_vault)?;
 
@@ -104,4 +76,96 @@ pub fn read_keyvault(file: &Path) -> Result<KeyVault> {
     let keyvault: KeyVault = serde_json::from_reader(rdr)?;
 
     Ok(keyvault)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encrypt_and_decrypt() {
+        let password = "password";
+        let mnemonic = "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong";
+        let hd_keys = HDkeys::from_mnemonic(mnemonic);
+
+        let seed_hex = "b6a6d8921942dd9806607ebc2750416b289adea669198769f2e15ed926c3aa92bf88ece232317b4ea463e84b0fcd3b53577812ee449ccc448eb45e6f544e25b6";
+        let seed: [u8; 64] = hex::decode(seed_hex)
+            .unwrap()
+            .as_slice()
+            .try_into()
+            .unwrap();
+
+        assert_eq!(hd_keys.seed, seed);
+        assert_eq!(hex::encode(hd_keys.seed), seed_hex);
+
+        let salt = SaltString::from_b64("M6lWvNAGuZBSp9fBGAUEqw").unwrap();
+        let nonce = "287189f34a1433d2de201d08";
+
+        let keyvault = hd_keys.get_keyvault(&salt, nonce, Some(password)).unwrap();
+
+        assert!(verify_password(&keyvault.valut.mac, password));
+
+        let decrypted_seed = decrypt_seed(
+            &keyvault.valut.cipher_text,
+            password,
+            &keyvault.valut.cipher_params.nonce,
+            &keyvault.valut.kdf_params,
+        )
+        .unwrap();
+
+        assert_eq!(hd_keys.seed, decrypted_seed);
+    }
+
+    #[test]
+    fn test_decrypt_keyvault() {
+        let keyvault: KeyVault = serde_json::from_str(
+            r#"
+            {
+                "valut": {
+                    "cipher": "aes256-gcm",
+                    "cipher_params": { "nonce": "287189f34a1433d2de201d08" },
+                    "cipher_text": "7a34170003c0a7b3ccb75bac28757801a7d9b5e1ff062afa4af5f3c03e7d8982eb1f36ccce87436e42b44ffea6bcf39eba8c15d4e79ee0bf012811fca81ae1e112c0f8ae5d8e43ac8cad1ae961b11207",
+                    "kdf": "pbkdf2",
+                    "kdf_params": {
+                    "key_length": 32,
+                    "n": 600000,
+                    "salt": "M6lWvNAGuZBSp9fBGAUEqw"
+                    },
+                    "mac": "$argon2id$v=19$m=19456,t=2,p=1$M6lWvNAGuZBSp9fBGAUEqw$/U5VYPmg3+BQj0ttOyPnOUjH7bP23V9/tgvBpovna/8",
+                    "salt": "M6lWvNAGuZBSp9fBGAUEqw"
+                },
+                "chain_keys": [
+                    {
+                    "chain": "Solana",
+                    "public_keys": ["AbwHhAquPXvDfxvWEh1b4mG969DQF9wJQSK5k8XKSKtG"]
+                    },
+                    {
+                    "chain": "EVM",
+                    "public_keys": ["0x50940F0C5779BE15F7ACB12E9b75128e1415BFec"]
+                    }
+                ]
+            }
+            "#,
+        ).unwrap();
+
+        let password = "password";
+        let seed_hex = "b6a6d8921942dd9806607ebc2750416b289adea669198769f2e15ed926c3aa92bf88ece232317b4ea463e84b0fcd3b53577812ee449ccc448eb45e6f544e25b6";
+        let seed: [u8; 64] = hex::decode(seed_hex)
+            .unwrap()
+            .as_slice()
+            .try_into()
+            .unwrap();
+
+        assert!(verify_password(&keyvault.valut.mac, password));
+
+        let decrypted_seed = decrypt_seed(
+            &keyvault.valut.cipher_text,
+            password,
+            &keyvault.valut.cipher_params.nonce,
+            &keyvault.valut.kdf_params,
+        )
+        .unwrap();
+
+        assert_eq!(seed, decrypted_seed);
+    }
 }

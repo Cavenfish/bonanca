9d8eda13b6d90c29cb233ab71a8b1d39d95b8ea7
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-12-02 10:24:34 +0100

    forgot to add core traits

---
+++ crates/bonanca-core/src/traits.rs
@@ -0,0 +1,61 @@
+use alloy::rpc::types::TransactionRequest;
+use alloy::signers::{k256::ecdsa::SigningKey, local::LocalSigner};
+use anyhow::Result;
+use async_trait::async_trait;
+use solana_sdk::signature::Keypair;
+use solana_sdk::transaction::VersionedTransaction;
+
+use super::holdings::Asset;
+
+pub enum SwapTransactionData {
+    Sol(VersionedTransaction),
+    Evm(TransactionRequest),
+}
+
+pub enum CryptoSigners {
+    Evm(LocalSigner<SigningKey>),
+    Sol(Keypair),
+}
+
+#[async_trait]
+pub trait Wallet {
+    fn get_pubkey(&self) -> Result<String>;
+
+    fn get_signer(&self) -> Result<CryptoSigners>;
+
+    fn parse_native_amount(&self, amount: f64) -> Result<u64>;
+
+    async fn parse_token_amount(&self, amount: f64, token: &str) -> Result<u64>;
+
+    async fn close(&self, to: &str) -> Result<()>;
+
+    async fn balance(&self) -> Result<f64>;
+
+    async fn transfer(&self, to: &str, amount: f64) -> Result<()>;
+
+    async fn token_balance(&self, token: &str) -> Result<f64>;
+
+    async fn transfer_token(&self, token: &str, amount: f64, to: &str) -> Result<()>;
+
+    async fn transfer_all_tokens(&self, token: &str, to: &str) -> Result<()>;
+
+    async fn check_swap(&self, token: &str, amount: f64, spender: Option<&str>) -> Result<bool>;
+
+    async fn swap(&self, swap_data: SwapTransactionData) -> Result<()>;
+}
+
+#[async_trait]
+pub trait Exchange {
+    async fn get_swap_data(
+        &self,
+        wallet: &Box<dyn Wallet + Send + Sync>,
+        sell: &str,
+        buy: &str,
+        amount: f64,
+    ) -> Result<SwapTransactionData>;
+}
+
+#[async_trait]
+pub trait Oracle {
+    async fn get_token_value(&self, asset: &Asset, amount: f64, chain: &str) -> Result<f64>;
+}

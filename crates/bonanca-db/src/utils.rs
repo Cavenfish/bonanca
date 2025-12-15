8a8433083d3091767f8e0262874ad0108304eb18
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-12-15 13:32:58 +0100

    forgot to add utils

---
+++ crates/bonanca-db/src/utils.rs
@@ -0,0 +1,58 @@
+use std::any::type_name;
+use std::fmt::Debug;
+use std::path::Path;
+
+use anyhow::Result;
+use bincode::{Decode, Encode, decode_from_slice, encode_to_vec};
+use redb::{Database, Value};
+
+pub fn create_db(db_file: &Path) -> Result<()> {
+    if !db_file.is_file() {
+        let _ = Database::create(&db_file)?;
+    }
+
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct Bincode<T>(pub T);
+
+impl<T> Value for Bincode<T>
+where
+    T: Debug + Encode + Decode<()>,
+{
+    type SelfType<'a>
+        = T
+    where
+        Self: 'a;
+
+    type AsBytes<'a>
+        = Vec<u8>
+    where
+        Self: 'a;
+
+    fn fixed_width() -> Option<usize> {
+        None
+    }
+
+    fn from_bytes<'a>(data: &'a [u8]) -> Self::SelfType<'a>
+    where
+        Self: 'a,
+    {
+        decode_from_slice(data, bincode::config::standard())
+            .unwrap()
+            .0
+    }
+
+    fn as_bytes<'a, 'b: 'a>(value: &'a Self::SelfType<'b>) -> Self::AsBytes<'a>
+    where
+        Self: 'a,
+        Self: 'b,
+    {
+        encode_to_vec(value, bincode::config::standard()).unwrap()
+    }
+
+    fn type_name() -> redb::TypeName {
+        redb::TypeName::new(&format!("Bincode<{}>", type_name::<T>()))
+    }
+}

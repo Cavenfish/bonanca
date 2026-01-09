5a601b6c24c71adc6bf0c349be811aa2d10110c6
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2026-01-09 22:29:25 +0100

    use submodule layout for python wrapper

--- crates/bonanca-python/src/lib.rs
+++ crates/bonanca-python/src/lib.rs
@@ -5,13 +5,24 @@ use pyo3::prelude::*;
 
 #[pymodule]
 mod bonanca {
+    #[pymodule_export]
+    use super::pywallets;
 
+    #[pymodule_export]
+    use super::pydefi;
+}
+
+#[pymodule(name = "wallets", submodule)]
+mod pywallets {
     #[pymodule_export]
     use crate::wallets::{
         evm::{PyEvmWallet, PyEvmWalletView},
         solana::{PySolWallet, PySolWalletView},
     };
+}
 
+#[pymodule(name = "defi", submodule)]
+mod pydefi {
     #[pymodule_export]
     use crate::defi::{evm::zerox::PyZeroX, solana::jupiter::PyJupiter};
 }

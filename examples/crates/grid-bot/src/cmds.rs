3ba9d40c3f8952cb85bd86bb7dc8c8d59fd9037b
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2026-01-19 22:38:50 +0100

    bugfix: correct level changing

--- examples/crates/grid-bot/src/cmds.rs
+++ examples/crates/grid-bot/src/cmds.rs
@@ -96,7 +96,7 @@ async fn maybe_buy(
                 let txn = dex.swap(&wallet, quote).await?;
                 println!("Buy Txn: {}", txn.block_hash.unwrap().to_string());
             }
-            *level = pair.upper_limit * 2.0;
+            *level = pair.lower_limit / 2.0;
             break;
         }
     }
@@ -131,7 +131,7 @@ async fn maybe_sell(
                 let txn = dex.swap(&wallet, quote).await?;
                 println!("Buy Txn: {}", txn.block_hash.unwrap().to_string());
             }
-            *level = pair.lower_limit / 2.0;
+            *level = pair.upper_limit * 2.0;
             break;
         }
     }

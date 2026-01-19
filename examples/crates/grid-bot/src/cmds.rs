541cf61e172e3f949a6ff2ca688311884da247aa
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2026-01-19 20:39:48 +0100

    calculate price from buy_amount rather than min_buy_amount

--- examples/crates/grid-bot/src/cmds.rs
+++ examples/crates/grid-bot/src/cmds.rs
@@ -85,8 +85,7 @@ async fn maybe_buy(
         .await
         .unwrap();
 
-    let out: f64 =
-        quote.min_buy_amount.parse::<f64>().unwrap() / 10.0_f64.powi(pair.token_b.decimals);
+    let out: f64 = quote.buy_amount.parse::<f64>().unwrap() / 10.0_f64.powi(pair.token_b.decimals);
     let price = 1.0 / (out / pair.sell_amount);
 
     for level in levels.iter_mut() {
@@ -121,8 +120,7 @@ async fn maybe_sell(
         .await
         .unwrap();
 
-    let out: f64 =
-        quote.min_buy_amount.parse::<f64>().unwrap() / 10.0_f64.powi(pair.token_a.decimals);
+    let out: f64 = quote.buy_amount.parse::<f64>().unwrap() / 10.0_f64.powi(pair.token_a.decimals);
     let price = out / pair.sell_amount;
 
     for level in levels.iter_mut() {
